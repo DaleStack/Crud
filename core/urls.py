a3fba12b8d1953871978dad19307dbf50f5988a6
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-08-22 17:01:01 +0800

    faet: listing note

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import settings
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('crud.urls')),
 ]
 
 if settings.DEBUG:
--- crud/urls.py
+++ crud/urls.py
@@ -3,7 +3,7 @@ from .views import create_note, list_note, update_note, delete_note
 
 urlpatterns = [
     path('create/', create_note, name='create_note'),
-    path('list/', list_note, name='list_note'),
+    path('', list_note, name='list_note'),
     path('update/<int:pk>/', update_note, name='update_note'),
     path('delete/<int:pk>/', delete_note, name='delete_note'),
 ]
